122ce18b650dbf4de42dd317b5e03557083a4859
Arima1213 <arima3992@gmail.com>
2025-04-10 10:31:23 +0700

    add product resource with enhanced attributes and CRUD functionality

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,12 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['category_id', 'name', 'description', 'price'];
+    protected $fillable = [
+        'category_id',
+        'itemcode',
+        'image',
+        'name',
+        'brand',
+        'itemweight',
+        'description',
+        'price'
+    ];
 
     public function category()
     {
--- database/migrations/2025_04_10_024756_create_products_table.php
+++ database/migrations/2025_04_10_024756_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->string('itemcode')->unique();
+            $table->string('image');
             $table->string('name');
             $table->string('brand')->nullable();
             $table->integer('itemweight')->nullable();
