c47d3bc38ff3d912d92dfe0245c33d65d3ff7a27
Arima1213 <arima3992@gmail.com>
2025-04-11 02:28:11 +0700

    add unit seeder to populate units table with predefined units and their abbreviations

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Unit;
+use App\Models\Units;
 
 class UnitSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $units = [
+            ['name' => 'Kilogram', 'short' => 'kg'],
+            ['name' => 'Gram', 'short' => 'g'],
+            ['name' => 'Liter', 'short' => 'l'],
+            ['name' => 'Mililiter', 'short' => 'ml'],
+            ['name' => 'Meter', 'short' => 'm'],
+            ['name' => 'Centimeter', 'short' => 'cm'],
+            ['name' => 'Milimeter', 'short' => 'mm'],
+            ['name' => 'Jam', 'short' => 'h'],
+            ['name' => 'Menit', 'short' => 'min'],
+            ['name' => 'Detik', 'short' => 's'],
+            ['name' => 'Hari', 'short' => 'd'],
+            ['name' => 'Minggu', 'short' => 'w'],
+            ['name' => 'Bulan', 'short' => 'mo'],
+            ['name' => 'Tahun', 'short' => 'y'],
+            ['name' => 'Unit', 'short' => 'unit'],
+            ['name' => 'Orang', 'short' => 'org'],
+            ['name' => 'Paket', 'short' => 'pkt'],
+            ['name' => 'Set', 'short' => 'set'],
+            ['name' => 'Lembar', 'short' => 'lbr'],
+            ['name' => 'Dus', 'short' => 'dus'],
+        ];
+
+        foreach ($units as $unit) {
+            Units::create($unit);
+        }
     }
 }
