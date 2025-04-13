7792505efa1f56143b89e8a3d337cc176f0875c9
Arima1213 <arima3992@gmail.com>
2025-04-13 18:58:48 +0700

    feat: add Customer resource with CRUD pages for managing customers

---
+++ app/Filament/Cashier/Resources/CustomerResource/Pages/EditCustomer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Cashier\Resources\CustomerResource\Pages;
+
+use App\Filament\Cashier\Resources\CustomerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCustomer extends EditRecord
+{
+    protected static string $resource = CustomerResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
