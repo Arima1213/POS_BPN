64d03b549e3343bdeac78d358a20355dd13c7d0e
Arima1213 <arima3992@gmail.com>
2025-04-14 04:59:17 +0700

    feat: enhance Procurement model with fillable attributes and relationships to Product and Supplier

--- app/Models/Procurement.php
+++ app/Models/Procurement.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Procurement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'quantity', 'price', 'procurement_date', 'supplier_id', 'note'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
