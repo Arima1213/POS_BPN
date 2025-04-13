5abed16d9db8d42522743edbc520545c5d246261
Arima1213 <arima3992@gmail.com>
2025-04-14 05:33:35 +0700

    feat: add fillable attributes and relationship to CompanyDebtPayment model

--- app/Models/CompanyDebtPayment.php
+++ app/Models/CompanyDebtPayment.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CompanyDebtPayment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['company_debt_id', 'amount', 'payment_date', 'note'];
+
+    public function companyDebt()
+    {
+        return $this->belongsTo(CompanyDebt::class);
+    }
 }
