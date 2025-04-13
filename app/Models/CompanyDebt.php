52694ecd4700f835ee92490990adebf2268ed6fd
Arima1213 <arima3992@gmail.com>
2025-04-14 05:00:17 +0700

    feat: add fillable attributes and relationships to CompanyDebt and ProductStock models

--- app/Models/CompanyDebt.php
+++ app/Models/CompanyDebt.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CompanyDebt extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['creditor_name', 'amount', 'paid', 'due_date', 'note'];
+
+    public function payments()
+    {
+        return $this->hasMany(CompanyDebtPayment::class);
+    }
+
+    public function remainingAmount()
+    {
+        return $this->amount - $this->paid;
+    }
 }
--- app/Models/ProductStock.php
+++ app/Models/ProductStock.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductStock extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'current_stock', 'minimum_stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
