67ac7960bf6572743bb9fbf48551fe4fb94409b1
Arima1213 <arima3992@gmail.com>
2025-04-14 03:48:54 +0700

    feat: add foreign key and stock management columns to product_stocks migration

--- database/migrations/2025_04_13_204826_create_product_stocks_table.php
+++ database/migrations/2025_04_13_204826_create_product_stocks_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('product_stocks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('current_stock')->default(0);
+            $table->integer('minimum_stock')->default(0);
             $table->timestamps();
         });
     }
