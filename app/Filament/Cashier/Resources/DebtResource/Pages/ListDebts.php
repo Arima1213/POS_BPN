2ac76697f9a6889f33772680e379465f975d6bf9
Arima1213 <arima3992@gmail.com>
2025-04-13 12:11:13 +0700

    feat: remove create action from Debt resource list and add DebtPayments model and migration

--- app/Filament/Cashier/Resources/DebtResource/Pages/ListDebts.php
+++ app/Filament/Cashier/Resources/DebtResource/Pages/ListDebts.php
@@ -12,8 +12,6 @@ class ListDebts extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        return [];
     }
 }
