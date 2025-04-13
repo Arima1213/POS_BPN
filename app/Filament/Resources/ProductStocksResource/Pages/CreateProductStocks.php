ebe014176ff122562a2d7c0c5e95ff5d3f27b950
Arima1213 <arima3992@gmail.com>
2025-04-14 06:03:54 +0700

    feat: add ProductStocks resource with CRUD pages and migration for product stocks table

---
+++ app/Filament/Resources/ProductStocksResource/Pages/CreateProductStocks.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ProductStocksResource\Pages;
+
+use App\Filament\Resources\ProductStocksResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProductStocks extends CreateRecord
+{
+    protected static string $resource = ProductStocksResource::class;
+}
