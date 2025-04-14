81dd2b764c810e5d6bfe82f41ee0df88f90d146d
Arima1213 <arima3992@gmail.com>
2025-04-14 07:25:50 +0700

    feat: add automatic stock creation for new products in CreateProduct page

--- app/Filament/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Resources/ProductResource/Pages/CreateProduct.php
@@ -3,10 +3,22 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
+use App\Models\ProductStock;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProduct extends CreateRecord
 {
     protected static string $resource = ProductResource::class;
+    protected function afterCreate(): void
+    {
+        // Cek jika stok produk belum ada untuk product_id tersebut
+        if (!ProductStock::where('product_id', $this->record->id)->exists()) {
+            ProductStock::create([
+                'product_id' => $this->record->id,
+                'current_stock' => 0,      // default awal 0
+                'minimum_stock' => 0,      // bisa kamu sesuaikan
+            ]);
+        }
+    }
 }
